819aa317df13d4453dc10a97e0b1855ddc81b48e
Kaan Seyitogullari <kaan.seyitogullari@zalando.ie>
2020-04-10 13:49:06 +0100

    update name

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,15 @@
 import typescript from 'rollup-plugin-typescript2'
 import pkg from './package.json'
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
-// import { terser } from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser'
 
 export default [
   {
     input: 'src/index.ts',
-    external: [
-      Object.keys(pkg.peerDependencies),
-      Object.keys(pkg.dependencies),
-    ],
+    external: [Object.keys(pkg.peerDependencies)],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'esm' },
-      {
-        file: 'example/src/libs/index.js',
-        format: 'es',
-        banner: '/* eslint-disable */',
-      },
     ],
     plugins: [
       typescript({
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-import 'intersection-observer'
 import useLocalStorage from './useLocalStorage/useLocalStorage'
 export default useLocalStorage
